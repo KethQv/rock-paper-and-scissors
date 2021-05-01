6ea9932be97ae9cd37914606884f4fc85602c08e
Kenneth Quintero <kennethgqv@gmail.com>
2021-05-01 00:43:02 -0600

    new implementation

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// const player = {
-//     lives: 5,
-//     choice: null,
-// };
+window.addEventListener('load', () => {
+    if (screen.width <= 500) {
+        let span_1 = document.querySelector('.span-dialog-1');
+        let span_2 = document.querySelector('.span-dialog-2');
+        span_1.classList += ' span-dialog--left-entry';
+        span_2.classList += ' span-dialog--right-entry';
 
-// const computer = {
-//     lives: 5,
-//     choice: null,
-// };
+        let choices = document.querySelector('.choices');
+        let wrapper = document.querySelector('.wrapper');
+        choices.classList += ' scene';
+        wrapper.classList += ' carousel carousel--entry';
 
-// const possibleChoices = ['rock', 'paper', 'scissors'];
-
-// // get selection with mouse click
-// const choices = document.querySelectorAll('.choice');
-// choices.forEach((element) => {
-//     element.addEventListener('click', function(e) {
-//         console.log(element.id);
-//     });
-// });
-
+        let choices_items = document.querySelectorAll('.choices__item');
+        choices_items[0].classList += ' carousel__item carousel__item--front';
+        choices_items[1].classList += ' carousel__item carousel__item--right';
+        choices_items[2].classList += ' carousel__item carousel__item--left';
+    } else {
+        let box_dialog = document.querySelector('.box-dialog');
+        box_dialog.classList += ' box-dialog--top-entry';
+    }
+});
